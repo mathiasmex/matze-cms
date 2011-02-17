795238ea280d902f2da0ea9039cb47c9ea9d433d
Mathias <mathiasmex@gmail.com>
2011-02-17 09:48:27 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vrame::Application.config.secret_token = '4246f3972f25a5afe305e4243d1aa64b2535bf780449a36ed3e7feae140d4e6a5bdb08b2ed14ca61aa401df9cc9c8c7c51ee354ce91be9719bea22bdb6b0c603'
